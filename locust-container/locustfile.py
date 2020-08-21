7c1711007f266712a49ad94385aeabbd7ea77a2e
Tom Ellis <ellistr@amazon.com>
2020-08-21 20:20:35 +0000

    fix old locust file

--- locust-container/locustfile.py
+++ locust-container/locustfile.py
@@ -1,25 +1,9 @@
-from locust import HttpLocust, TaskSet, task, between
+import time
+from locust import HttpUser, task, between
 
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
 
-def index(l):
-    l.client.get("/")
-
-def stats(l):
-    l.client.get("/stats/requests")
-
-class UserTasks(TaskSet):
-    # one can specify tasks like this
-    tasks = [index, stats]
-    
-    # but it might be convenient to use the @task decorator
     @task
-    def page404(self):
-        self.client.get("/does_not_exist")
-    
-class WebsiteUser(HttpLocust):
-    """
-    Locust user class that does requests to the locust web server running on localhost
-    """
-    #host = "http://127.0.0.1:8089"
-    wait_time = between(2, 5)
-    task_set = UserTasks
+    def index_page(self):
+        self.client.get("/")
