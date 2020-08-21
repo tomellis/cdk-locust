761bd1de0ccd7f3ac127c6210adc694866e8ff10
Tom Ellis <ellistr@amazon.com>
2020-08-21 17:37:30 +0000

    add initial infra

--- lib/locust-cdk-fargate-stack.ts
+++ lib/locust-cdk-fargate-stack.ts
@@ -1,9 +1,46 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
+import * as cw from '@aws-cdk/aws-cloudwatch';
+import * as path from 'path';
 
 export class LocustCdkFargateStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Build the VPC
+    const vpc = new ec2.Vpc(this, 'LocustFargateVPC', {
+      maxAzs: 3, // Default is all AZs in region
+    });
+
+    // ECS Cluster
+    const cluster = new ecs.Cluster(this, 'LocustFargateCluster', {
+      vpc: vpc,
+    });
+
+    // Task Definition
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'LocustTaskDefinition', {
+      memoryLimitMiB: 8192,
+      cpu: 4096
+    });
+
+    // Build and define the Container
+    const container = taskDefinition.addContainer('LocustContainer', {
+      image: ecs.ContainerImage.fromAsset(path.join(__dirname, '..', 'locust-container')),
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'LocustCdkFargate' }),
+      environment: {'TARGET_URL': '127.0.0.1'}
+    });
+    // Add port to container definition
+    container.addPortMappings({containerPort: 8089});
+
+    // Setup service
+    const loadBalancedFargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'Locust', {
+      cluster,
+      memoryLimitMiB: 8192,
+      cpu: 4096,
+      taskDefinition: taskDefinition
+    });
+
   }
 }
